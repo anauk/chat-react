c0fe646005e7ab74c41e8721ab0cefafd4310702
marinasavluchinska <anauk2018@gmail.com>
2019-09-08 03:02:25 +0300

    transported UsersAsClass to UsersContainer

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -2,7 +2,38 @@ import React from 'react'
 import {connect} from "react-redux";
 import Users from "./Users";
 import {followAC, unfollowAC, setUsersAC, setCurrentPageAC, setUsersCountAC} from "../../redux/users-reducer";
-import UserAsClass from "./UserAsClass";
+import * as axios from "axios";
+
+class UserAsClass extends React.Component {
+  componentDidMount() {
+    axios.get(`https://swapi.co/api/people?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+      .then(response => {
+        this.props.setUsers(response.data.results)
+        this.props.setCount(response.data.count)
+      })
+  }
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber)
+    axios.get(`https://swapi.co/api/people?page=${pageNumber}&count=${this.props.pageSize}`)
+      .then(response => {
+        this.props.setUsers(response.data.results)
+      })
+  }
+
+  render() {
+    return (
+      <Users
+        count={this.props.count}
+        pageSize={this.props.pageSize}
+        onPageChanged={this.onPageChanged}
+        currentPage={this.props.currentPage}
+        users={this.props.users}
+        follow={this.props.follow}
+        unfollow={this.props.unfollow}
+      />
+    )
+  }
+};
 
 const mapStateToProps = (state) => {
   return {
@@ -30,7 +61,7 @@ const mapDispatchToProps = (dispatch) => {
       dispatch(setUsersCountAC(count))
     }
   }
-}
-let UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UserAsClass)
+};
+let UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UserAsClass);
 
 export default UsersContainer
